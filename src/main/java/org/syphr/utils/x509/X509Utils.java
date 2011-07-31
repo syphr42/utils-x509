0a6868784b4144ad28646de2a5459fc355341cda
Gregory Moyer <moyerg@syphr.com>
2011-07-30 21:03:55 -0400

    initial project commit

---
+++ src/main/java/org/syphr/utils/x509/X509Utils.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright 2011 Gregory P. Moyer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.syphr.utils.x509;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+
+import org.apache.commons.io.IOUtils;
+
+/**
+ * This utility makes it easy to create and verify signatures using an X.509
+ * key/certificate pair.
+ *
+ * @author Gregory P. Moyer
+ */
+public class X509Utils
+{
+    /**
+     * The certificate factory to use. This will be lazily created on demand.
+     */
+    private static volatile CertificateFactory CERT_FACTORY;
+
+    /**
+     * Create a signature using the given token and private key.
+     *
+     * @param token
+     *            the token to sign
+     * @param key
+     *            the private key to use to create the signature (this must be
+     *            PKCS8 encoded)
+     * @param keyAlg
+     *            the algorithm used to create the private key
+     * @param sigAlg
+     *            the algorithm to use to create the signature
+     * @return the signature
+     * @throws IOException
+     *             if there is an error reading the key
+     * @throws InvalidKeySpecException
+     *             if the key algorithm is not appropriate for the given private
+     *             key
+     * @throws InvalidKeyException
+     *             if the given private key is not valid
+     * @throws SignatureException
+     *             if there is an error while generating the signature
+     */
+    public static byte[] sign(String token,
+                              InputStream key,
+                              KeyAlgorithm keyAlg,
+                              SignatureAlgorithm sigAlg) throws IOException,
+                                                        InvalidKeySpecException,
+                                                        InvalidKeyException,
+                                                        SignatureException
+    {
+        /*
+         * This will need to be modified if more key algorithms are added. This
+         * will only work for PKCS8 encoded RSA private keys.
+         */
+        KeySpec privateKeySpec = new PKCS8EncodedKeySpec(IOUtils.toByteArray(key));
+
+        try
+        {
+            KeyFactory keyFactory = KeyFactory.getInstance(keyAlg.getAlgorithm());
+            PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
+
+            Signature sig = Signature.getInstance(sigAlg.getAlgorithm());
+            sig.initSign(privateKey);
+            sig.update(token.getBytes());
+            return sig.sign();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            /*
+             * This is protected against by enforcing specific algorithm
+             * choices.
+             */
+            throw new IllegalArgumentException("Unknown algorithm", e);
+        }
+    }
+
+    /**
+     * Verify a signature using the given token and certificate.
+     *
+     * @param token
+     *            the token to which the signature belongs
+     * @param signature
+     *            the signature to verify
+     * @param sigAlg
+     *            the algorithm used to create the signature
+     * @param certificate
+     *            the certificate to use to verify the signature
+     * @return <code>true</code> if the signature is valid; <code>false</code>
+     *         otherwise
+     * @throws CertificateException
+     *             if there is an error reading the certificate
+     * @throws InvalidKeyException
+     *             if the given certificate does not have a valid public key
+     * @throws SignatureException
+     *             if the signature is not valid
+     */
+    public static boolean verify(String token,
+                                 byte[] signature,
+                                 SignatureAlgorithm sigAlg,
+                                 InputStream certificate) throws CertificateException,
+                                                         InvalidKeyException,
+                                                         SignatureException
+    {
+        Certificate cert = getCertFactory().generateCertificate(certificate);
+
+        try
+        {
+            Signature sig = Signature.getInstance(sigAlg.getAlgorithm());
+            sig.initVerify(cert);
+            sig.update(token.getBytes());
+            return sig.verify(signature);
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            /*
+             * This is protected against by enforcing specific algorithm
+             * choices.
+             */
+            throw new IllegalArgumentException("Unknown algorithm", e);
+        }
+    }
+
+    /**
+     * Retrieve (and possible create) the certificate factory to use when
+     * reading certificates.
+     *
+     * @return the certificate factory
+     * @throws CertificateException
+     *             if no X.509 certificate provider is found
+     */
+    private static CertificateFactory getCertFactory() throws CertificateException
+    {
+        if (CERT_FACTORY == null)
+        {
+            synchronized (X509Utils.class)
+            {
+                if (CERT_FACTORY == null)
+                {
+                    CERT_FACTORY = CertificateFactory.getInstance("X.509");
+                }
+            }
+        }
+
+        return CERT_FACTORY;
+    }
+
+    private X509Utils()
+    {
+        /*
+         * Static utilities
+         */
+    }
+}
---
+++ src/test/java/org/syphr/utils/x509/X509UtilsTest.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2011 Gregory P. Moyer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.syphr.utils.x509;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.SignatureException;
+import java.security.cert.CertificateException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+
+import junit.framework.Assert;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.syphr.utils.x509.KeyAlgorithm;
+import org.syphr.utils.x509.SignatureAlgorithm;
+import org.syphr.utils.x509.X509Utils;
+
+public class X509UtilsTest
+{
+    private static final String KEY_RESOURCE = "/cert/test.pkcs8";
+    private static final String CERT_RESOURCE = "/cert/test.cert";
+
+    private static final String SIG_RESOURCE = "/sig/sig.data";
+
+    private static final String TOKEN = "qwertyuioplkjhgfdsazxcvbnm0987654321";
+
+    @Test
+    public void testSign() throws IOException,
+                          InvalidKeyException,
+                          InvalidKeySpecException,
+                          SignatureException
+    {
+        InputStream key = getKey();
+
+        try
+        {
+            byte[] actual = X509Utils.sign(TOKEN,
+                                           key,
+                                           KeyAlgorithm.RSA,
+                                           SignatureAlgorithm.MD5_RSA);
+
+            InputStream expectedSteam = getSigExpected();
+            try
+            {
+                byte[] expected = IOUtils.toByteArray(expectedSteam);
+                Assert.assertTrue(Arrays.equals(expected, actual));
+            }
+            finally
+            {
+                expectedSteam.close();
+            }
+        }
+        finally
+        {
+            key.close();
+        }
+    }
+
+    @Test
+    public void testVerify() throws IOException,
+                            CertificateException,
+                            InvalidKeyException,
+                            SignatureException,
+                            InvalidKeySpecException
+    {
+        InputStream key = getKey();
+        InputStream cert = getCert();
+
+        try
+        {
+            try
+            {
+                Assert.assertTrue(X509Utils.verify(TOKEN,
+                                                   X509Utils.sign(TOKEN,
+                                                                  key,
+                                                                  KeyAlgorithm.RSA,
+                                                                  SignatureAlgorithm.MD5_RSA),
+                                                   SignatureAlgorithm.MD5_RSA,
+                                                   cert));
+            }
+            finally
+            {
+                key.close();
+            }
+        }
+        finally
+        {
+            cert.close();
+        }
+    }
+
+    private InputStream getKey() throws IOException
+    {
+        return getResource(KEY_RESOURCE);
+    }
+
+    private InputStream getCert() throws IOException
+    {
+        return getResource(CERT_RESOURCE);
+    }
+
+    private InputStream getSigExpected() throws IOException
+    {
+        return getResource(SIG_RESOURCE);
+    }
+
+    private InputStream getResource(String resource) throws IOException
+    {
+        InputStream is = getClass().getResourceAsStream(resource);
+
+        if (is == null)
+        {
+            throw new IOException("Unable to find resource: " + resource);
+        }
+
+        return is;
+    }
+}
